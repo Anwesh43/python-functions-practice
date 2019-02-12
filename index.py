1b5f4606c39071afc08ea15f3afd4ce638af6501
Anwesh43 <anweshthecool0@gmail.com>
2019-02-12 11:05:21 +0530

    adding closure

--- index.py
+++ index.py
@@ -10,7 +10,17 @@ def sum(a, b):
 def printSumNormal():
     print('sum is {0}'.format(sum(30, 40)))
 
-function_dictionary = {"basic":echo, "psn" : printSumNormal}
+def raiseTo(x):
+    def inner(n):
+        return x ** n
+    return inner
+
+def printCloure():
+    print("raising 2 to 3 and 4 times respectively")
+    print(raiseTo(2)(3))
+    print(raiseTo(2)(4))
+
+function_dictionary = {"basic":echo, "psn" : printSumNormal, "pc" : printCloure}
 if __name__ == "__main__":
 
     args = sys.argv
